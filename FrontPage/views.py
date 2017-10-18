132f54c4037530d4bb8c13390f5dca1f90a24503
derek.zhang <923770317@qq.com>
2017-10-18 17:30:24 +0800

    s

--- FrontPage/views.py
+++ FrontPage/views.py
@@ -9,11 +9,12 @@ sys.path.append("/util")
 from util import oracleUtil as ou
 from util import toolsUtil
 from django.http import JsonResponse
+from cx_Oracle import DatabaseError
 # Create your views here.
+
 
 # releaseVersion's index
 def releaseIndex(request):
-
     modelName = request.GET.get("searchText")
     reObjects = None
     if modelName:
@@ -44,103 +45,126 @@ def index(request):
 #delete the password
 def deletePassWord(request):
     if str(request.method) == "POST":
+        status = '000'
+        message = 'DELETE PASSWORD SUCCESS'
         member_id = request.POST.get("memberId")
         env = request.POST.get("env")
-        oracle_Util = None
-        resultCode = ""
-        if member_id is None:
-            resultCode = "member_id is required"
-        if env == "test":
-            oracle_Util = ou.oracleUtil("test")
-        else:
-            oracle_Util = ou.oracleUtil("inte")
-        resultCode = oracle_Util.deletePassWord(member_id)
-        return HttpResponse(resultCode)
-
+        if not member_id:
+            return JsonResponse({'status': '777', 'message': 'member_id is null'})
+        try:
+            ou.oracleUtil.deletePassWord(member_id,env)
+        except DatabaseError as e:
+            status = '999'
+            message = str(e)
+            return JsonResponse({'status': status, 'message': message})
+        except Exception as e :
+            status = '999'
+            message = str(e)
+        ou.cursor.close()
+        ou.conn.close()
+        return JsonResponse({'status':status,'message':message})
     else:
         return render_to_response("deletePassWord.html")
 
 
 #delete the cert
 def deleteCert(request):
-    status ='000'
-    message = ''
     if str(request.method) == "POST":
+        status = '000'
+        message = 'DELETE CERT SUCCESS'
         member_id = request.POST.get("memberId")
         env = request.POST.get("env")
         if not member_id:
             return JsonResponse({'status': '777', 'message': 'member_id is null'})
         try:
             ou.oracleUtil.deleteCertByMemberId(member_id,env)
+        except DatabaseError as e:
+            status = '999'
+            message = str(e)
+            return JsonResponse({'status': status, 'message': message})
         except Exception as e :
             status = '999'
             message = str(e)
-
-        finally:
-            ou.cursor.close()
-            ou.conn.close()
-            return JsonResponse({'status':'000','message':'delete success'})
+        ou.cursor.close()
+        ou.conn.close()
+        return JsonResponse({'status':status,'message':message})
     else:
         return render_to_response("deleteCert.html")
-
 
 #update the account's amount
 def updateAccount(request):
     if str(request.method) == 'POST':
+        status = '000'
+        message = 'UPDATE ACCOUNT SUCCESS'
         member_id = request.POST.get("memberId")
         env = request.POST.get("env")
         amount = request.POST.get('amount')
-        oracle_Util = None
-        resultCode = ""
-        if member_id is None or amount is None:
-            return 'pls enter the para'
-
-        if env == "test":
-            oracle_Util = ou.oracleUtil("test")
-        else:
-            oracle_Util = ou.oracleUtil("inte")
-        resultCode = oracle_Util.updateAccountByMemberId(member_id,amount)
-        toolsUtil.refreshAccountByMemberId(member_id,env)
-        return HttpResponse(resultCode)
+        if member_id == '' or amount  == '':
+            return JsonResponse({'status':'999','message':'PLS ENTER THE PARAM'})
+        try:
+            ou.oracleUtil.updateAccountByMemberId(member_id,amount,env)
+            toolsUtil.refreshAccountByMemberId(member_id, env)
+        except DatabaseError as e:
+            status = '999'
+            message = str(e)
+            return JsonResponse({'status': status, 'message': message})
+        except Exception as e :
+            status = '999'
+            message = str(e)
+        ou.cursor.close()
+        ou.conn.close()
+        return JsonResponse({'status':status,'message':message})
     return render_to_response("updateAccount.html")
+
 
 #udpate the receipt's amount
 def updateReceiptAmount(request):
     if str(request.method) == 'POST':
+        status = '000'
+        message = "UPDATE RECEIPT'S AMOUNT SUCCESS"
         receipt_id = request.POST.get('receiptId')
         env = request.POST.get('env')
         amount = request.POST.get('amount')
-        oracle_Util = None
-        resultCode = ""
-        if receipt_id is None or amount is None:
-            return 'pls enter the para'
+        if receipt_id == '' or amount == '':
+            return JsonResponse({'status': '999', 'message': 'PLS ENTER THE PARAM'})
 
-        if env == "test":
+        try:
+            ou.oracleUtil.updateReceiptById(receipt_id, amount,env)
+            toolsUtil.refreshReceiptById(receipt_id, env)
+        except DatabaseError as e:
+            status = '999'
+            message = str(e)
+            return JsonResponse({'status': status, 'message': message})
 
-            oracle_Util = ou.oracleUtil("test")
-        else:
-            oracle_Util = ou.oracleUtil("inte")
+        except Exception as e:
+            status = '999'
+            message = str(e)
 
-        resultCode = oracle_Util.updateReceiptById(receipt_id, amount)
-        toolsUtil.refreshReceiptById(receipt_id, env)
-        return HttpResponse(resultCode)
+        ou.cursor.close()
+        ou.conn.close()
+        return JsonResponse({'status': status, 'message': message})
     return render_to_response('updateReceipt.html')
 
 
+#接口授权
 def interfaceGrant(request):
     if str(request.method) == 'POST':
         partner_id = request.POST.get("partner_id")
         service_code = request.POST.get('service_code')
         env = request.POST.get('env')
-        oracle_Util = None
-        resultCode = ''
         if service_code == '' or partner_id == '':
-            return 'pls enter the para'
-        if env == "test":
+            return JsonResponse({'status': '999', 'message': 'PLS ENTER THE PARAM'})
+        try:
+            ou.oracleUtil.interfaceGrant(partner_id,service_code,env)
+        except DatabaseError as e:
+            status = '999'
+            message = str(e)
+            return JsonResponse({'status': status, 'message': message})
+        except Exception as e:
+            status = '999'
+            message = str(e)
 
-            oracle_Util = ou.oracleUtil("test")
-        else:
-            oracle_Util = ou.oracleUtil("inte")
-        resultCode = oracle_Util.interfaceGrant(partner_id,service_code)
-        return HttpResponse(resultCode)
+        ou.cursor.close()
+        ou.conn.close()
+        return JsonResponse({'status': status, 'message': message})
     return render_to_response('interfaceGrant.html')
--- util/oracleUtil.py
+++ util/oracleUtil.py
@@ -5,18 +5,7 @@ class oracleUtil(object):
     conn = None
     cursor = None
 
-    # def __init__(self,env):
-    #     try:
-    #         self.conn = None
-    #         if env == "test":
-    #             oracleUtil.conn = cx_Oracle.connect('wangbing','wangbing123','10.0.35.1:1521/testdb')
-    #         else:
-    #             oracleUtil.conn = cx_Oracle.connect('wangbing','wangbing123','10.0.35.1:1521/intedb')
-    #         oracleUtil.cursor = self.conn.cursor()
-    #     except Exception, e:
-    #         # self.cursor.close()
-    #         # self.conn.close()
-    #         print 'connect the oracle error'
+
 
     @staticmethod
     def initConandCursor(env):
@@ -35,92 +24,53 @@ class oracleUtil(object):
 
 
 
-
-    def deletePassWord(self,member_id):
-        resultCode = '000'
-        try:
-            sql = '''update mb.tb_operator t set t.pay_password = null ,t.lock_time = null ,t.last_error_time = null,
+    @staticmethod
+    def deletePassWord(member_id,env):
+        oracleUtil.initConandCursor(env)
+        sql = '''update mb.tb_operator t set t.pay_password = null ,t.lock_time = null ,t.last_error_time = null,
 t.password_error_count = null,t.encrypt_strategy = null where t.member_id =%s''' % member_id
-            self.cursor.execute(sql)
-            self.conn.commit()
-        except Exception, e:
-            print "delete password err", str(e)
-            resultCode = '999'
-        finally:
-            self.cursor.close()
-            self.conn.close()
-            return resultCode
+        oracleUtil.cursor.execute(sql)
+        oracleUtil.conn.commit()
 
-    def updateAccountByMemberId(self,memberId,amount):
-        resultCode = '000'
-        try:
-            sql = 'update account.tb_account t set t.available_balance = %s where t.member_id = %s and t.account_type_id = 1' % (amount,memberId)
-            self.cursor.execute(sql)
-            self.conn.commit()
+    @staticmethod
+    def updateAccountByMemberId(memberId,amount,env):
+        oracleUtil.initConandCursor(env)
+        sql = 'update account.tb_account t set t.available_balance = %s where t.member_id = %s and t.account_type_id = 1' % (amount,memberId)
+        oracleUtil.cursor.execute(sql)
+        oracleUtil.conn.commit()
 
-        except Exception, e:
-            print "update member's amount err", str(e)
-            resultCode = '999'
+    @staticmethod
+    def updateReceiptById(receiptId,amount,env):
+        oracleUtil.initConandCursor(env)
+        sql = 'update account.tb_receipt t set t.available_balance = %s where t.id = %s' % (amount, receiptId)
+        oracleUtil.cursor.execute(sql)
+        oracleUtil.conn.commit()
 
-        finally:
-            self.cursor.close()
-            self.conn.close()
-            return resultCode
-
-    def updateReceiptById(self,receiptId,amount):
-        resultCode = '000'
-        try:
-            sql = 'update account.tb_receipt t set t.available_balance = %s where t.id = %s' % (
-            amount, receiptId)
-            self.cursor.execute(sql)
-            self.conn.commit()
-
-        except Exception, e:
-            print "udpate receipt's amount err", str(e)
-            resultCode = '999'
-
-        finally:
-            self.cursor.close()
-            self.conn.close()
-            return resultCode
-
-    def getServiceId(self,service_code):
+    @staticmethod
+    def getServiceId(service_code):
         serviceId = ''
-        try:
-            sql = "select service_id from gateway.tb_partner_service where code = '%s' and rownum <=1" % service_code
-            self.cursor.execute(sql)
-            while (1):
-                row = self.cursor.fetchone()
-                if row == None:
-                    break
-                serviceId = row[0]
-
-        except Exception, e:
-            print "select err", str(e)
-            resultCode = ''
-
-        finally:
-            return str(serviceId)
+        sql = "select service_id from gateway.tb_partner_service where code = '%s' and rownum <=1" % service_code
+        oracleUtil.cursor.execute(sql)
+        while (1):
+            row = oracleUtil.cursor.fetchone()
+            if row == None:
+                break
+            serviceId = row[0]
+        return str(serviceId)
 
 
 
 
-    def interfaceGrant(self,partner_id,service_code):
-        resultCode = '000'
-        try:
-            serviceId = self.getServiceId(service_code)
-            if not serviceId:
-                return '999'
-            sql = "insert into gateway.tb_partner_service values(%s,%s,'%s',1)" %(partner_id,serviceId,service_code)
-            self.cursor.execute(sql)
-            self.conn.commit()
-        except Exception, e:
-            print "GRANT ERR", str(e)
-            resultCode = '999'
-        finally:
-            self.cursor.close()
-            self.conn.close()
-            return resultCode
+
+    @staticmethod
+    def interfaceGrant(partner_id,service_code,env):
+        oracleUtil.initConandCursor(env)
+        serviceId = oracleUtil.getServiceId(service_code)
+        if not serviceId:
+            raise Exception('no serveice_id')
+        sql = "insert into gateway.tb_partner_service values(%ks,%s,'%s',1)" %(partner_id,serviceId,service_code)
+        oracleUtil.cursor.execute(sql)
+        oracleUtil.conn.commit()
 
 
 
