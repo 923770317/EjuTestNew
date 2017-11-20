e80b6c4f25cbfc896333499d6461c47f15914d49
derek.zhang <923770317@qq.com>
2017-11-20 11:00:52 +0800

    s

--- EjuTest/settings.py
+++ EjuTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'v1k8wbu-1%ryq_5+ph3@4ecpdjde5f79myfnji!5#*$*z#=q!t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.36.1']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
